893eb9d63d6651bd3e56e684c11c3bbeded935ff
qlmio <microsoftedgeql@gmail.com>
2025-02-23 11:14:59 +0800

    fix

--- app/src/main/java/io/github/xsheeee/cs_controller/BaseActivity.kt
+++ app/src/main/java/io/github/xsheeee/cs_controller/BaseActivity.kt
@@ -3,7 +3,6 @@ package io.github.xsheeee.cs_controller
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.github.xsheeee.cs_controller.Tools.Logger
-import java.util.logging.Logger
 
 open class BaseActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
